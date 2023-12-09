618380c1f4d913bfa8c9a451ec96cdd84fee3d95
Arnaud Renaud <arnaud.renaud@gmail.com>
2023-12-09 19:28:55 +0100

    Install Framer Motion 5 and set up page transition animation

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,8 @@
 import * as React from "react";
 import { Helmet } from "react-helmet";
+
+import { motion } from "framer-motion";
+
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import "../style/bulma-style.sass";
@@ -50,7 +53,19 @@ const TemplateWrapper = ({ children }) => {
         />
       </Helmet>
       <Navbar />
-      <div>{children}</div>
+      <motion.div
+        initial={{ opacity: 0, x: -200 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: 200 }}
+        transition={{
+          type: "spring",
+          mass: 0.35,
+          stiffness: 75,
+          duration: 0.3,
+        }}
+      >
+        {children}
+      </motion.div>
       <Footer />
     </div>
   );
